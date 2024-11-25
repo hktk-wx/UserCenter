270d7a2c68673f5cfdaa6cc45a33a3f6eed2abec
hktk <2609420423@qq.com>
2024-11-25 16:47:48 +0800

    用户登录逻辑

--- src/main/java/com/wx/usercenter/model/vo/LoginUserVO.java
+++ src/main/java/com/wx/usercenter/model/vo/LoginUserVO.java
@@ -1,7 +1,5 @@
 package com.wx.usercenter.model.vo;
 
-import com.baomidou.mybatisplus.annotation.IdType;
-import com.baomidou.mybatisplus.annotation.TableId;
 import lombok.Data;
 
 import java.io.Serializable;
